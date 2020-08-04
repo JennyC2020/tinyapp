6bf6feef4b71c519f5f7758673f7d6040b634236
Jenny Citherlet <jennycith@gmail.com>
2020-08-04 21:52:36 +0000

    Added DELETE URL feature

--- express_server.js
+++ express_server.js
@@ -44,6 +44,11 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls'); //redirect the client back to the url_index page
+});
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
