02d4822e67beff776bbace0b8a6c3dfa81aa9567
Jenny Citherlet <jennycith@gmail.com>
2020-08-04 00:41:51 +0000

    Fixed urls issue

--- express_server.js
+++ express_server.js
@@ -17,12 +17,16 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-app.get('/urls:shortURL', (req, res) => {
+app.get('/urls/:shortURL', (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   };
   res.render('urls_show', templateVars);
+});
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {
