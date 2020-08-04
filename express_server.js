d2eb526bffb719a62c98870ab45bf5b97e33104a
Jenny Citherlet <jennycith@gmail.com>
2020-08-04 22:44:46 +0000

    Added URL edit feature

--- express_server.js
+++ express_server.js
@@ -16,21 +16,11 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//GET requests
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-});
-
-app.post("/urls", (req, res) => {
-  let shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(`/urls/${shortURL}`);
-  app.get("/urls/:shortURL", (req, res) => {
-    let shortURL = req.params.shortURL;
-    let longURL = urlDatabase[req.params.shortURL];
-    let templateVars = { shortURL, longURL };
-    res.render("urls_show", templateVars);
-  });
 });
 
 app.get("/urls/new", (req, res) => {
@@ -42,11 +32,6 @@ app.get("/urls/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
   let templateVars = { shortURL, longURL };
   res.render("urls_show", templateVars);
-});
-
-app.post('/urls/:shortURL/delete', (req, res) => {
-  delete urlDatabase[req.params.shortURL];
-  res.redirect('/urls'); //redirect the client back to the url_index page
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -65,6 +50,41 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+
+
+// POST Request
+
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+  app.get("/urls/:shortURL", (req, res) => {
+    let shortURL = req.params.shortURL;
+    let longURL = urlDatabase[req.params.shortURL];
+    let templateVars = { shortURL, longURL };
+    res.render("urls_show", templateVars);
+  });
+});
+
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls'); //redirect the client back to the url_index page
+});
+
+app.post('/urls/:shortURL', (req, res) => {
+  res.redirect(`/urls/${req.params.shortURL}`);
+});
+
+app.post('/urls/:shortURL/edit', (req, res) => {
+  const newURL = req.body.updateURL;
+  const shortURL = req.params.shortURL;
+  urlDatabase[shortURL] = newURL;
+  res.redirect('/urls');
+});
+
+
+// LISTEN requests
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
