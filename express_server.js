ccd11919cc2abe59f420d258521a86fc2a344486
Jenny Citherlet <jennycith@gmail.com>
2020-08-05 20:31:48 +0000

    Created users object

--- express_server.js
+++ express_server.js
@@ -22,16 +22,16 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 const users = {
-  //   "userRandomID": {
-  //     id: "userRandomID", 
-  //     email: "user@example.com", 
-  //     password: "purple-monkey-dinosaur"
-  //   },
-  //  "user2RandomID": {
-  //     id: "user2RandomID", 
-  //     email: "user2@example.com", 
-  //     password: "dishwasher-funk"
-  //   }
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
 };
 
 //GET requests
